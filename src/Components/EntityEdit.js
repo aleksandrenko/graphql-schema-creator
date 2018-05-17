6b3cbc00be6a324df94671c1c6f1bbcf349dc8ab
Nikolai Aleksandrenko <aleksandrenko@gmail.com>
2018-05-17 12:30:57 +0300

    Styling and connecting prop ui fields

--- src/Components/EntityEdit.js
+++ src/Components/EntityEdit.js
@@ -18,50 +18,107 @@ class EntityEdit extends Component {
         }
     }
 
-    onPropertyDeleteClick = (prop) => {
+    isSelected = (prop) => this.state.selectedProperty && this.state.selectedProperty.id === prop.id;
+
+    onPropertyDeleteClick = (prop, e) => {
         store.selected.deleteProperty(prop);
+        e.preventDefault();
+        e.stopPropagation();
     };
 
     onAddNewProperty = () => {
-        store.selected.addProperty();
+        //#PERF postpone the update
+        setTimeout(() => {
+            store.selected.addProperty();
+        }, 0);
+    };
+
+    onColorChange = (e) => {
+        store.selected.color = e.target.value;
+    };
+
+    onNameChange = (val) => {
+        //#PERF postpone the update
+        setTimeout(() => {
+            store.selected.name = val;
+        }, 0);
+    };
+
+    onPropertySelection = (prop) => {
+        const selectedProperty = this.state.selectedProperty && this.state.selectedProperty.id === prop.id
+        ? null
+        : prop;
+
+        this.setState({
+            selectedProperty
+        });
     };
 
     render() {
         const entity = store.selected;
+        const selectedProperty = this.state.selectedProperty;
 
         return (entity &&
-            <div className="form">
-                {
-                    entity.color &&
-                    <input type="color" value={entity.color} onChange={ (e) => { entity.color = e.target.value; } } />
-                }
+            <div className="edit-entity">
 
-                <TextField label="Name" value={entity.name} onChanged={ (val) => entity.name = val } />
-
-                <div>({entity.type})</div>
-
-                <DefaultButton primary={true} onClick={ this.onAddNewProperty }>Add property</DefaultButton>
-
-                <hr/>
-                <ul>
+                <div className="entity-name">
                     {
-                        entity.properties.map(prop => (
-                            <li key={prop.id}>
-                                <span onClick={ () => { this.setState({ selectedProperty: prop }) } }>{prop.name}</span>
-                                <IconButton
-                                    iconProps={ { iconName: 'Delete' } }
-                                    title='Delete'
-                                    onClick={ this.onPropertyDeleteClick.bind(this, prop) }
-                                />
-                            </li>
-                        ))
+                        entity.color &&
+                        <span className="color-selector">
+                            <input
+                                type="color"
+                                value={entity.color}
+                                onChange={this.onColorChange}
+                            />
+                        </span>
                     }
-                </ul>
 
-                { this.state.selectedProperty &&
-                    <Property data={this.state.selectedProperty} />
-                }
+                    <TextField
+                        className="entity-name-input"
+                        value={entity.name}
+                        onChanged={ this.onNameChange }
+                    />
 
+                    <div className="entity-name-type">({entity.type})</div>
+
+                    <DefaultButton
+                        iconProps={ { iconName: 'AddTo' } }
+                        onClick={ this.onAddNewProperty }
+                        className="create-property-btn"
+                    >
+                        Property
+                    </DefaultButton>
+                </div>
+
+                <div className="properties-wrapper">
+                    <ul className="properties-list">
+                        {
+                            entity.properties.map(prop => (
+                                <li
+                                    key={prop.id}
+                                    className={( this.isSelected(prop) ? "selected" : '' )}
+                                >
+                                    <div
+                                        className="property-list-name"
+                                        onClick={ this.onPropertySelection.bind(this, prop) }>
+                                        {prop.name}
+
+                                        <IconButton
+                                            className="property-list-delete-btn"
+                                            iconProps={ { iconName: 'Delete' } }
+                                            title='Delete'
+                                            onClick={ this.onPropertyDeleteClick.bind(this, prop) }
+                                        />
+                                    </div>
+
+                                    { (selectedProperty && selectedProperty.id === prop.id) &&
+                                        <Property data={selectedProperty} />
+                                    }
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
             </div>
         )
     }
